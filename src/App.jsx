2d28cd8ff77f265d6f2955b01a62c4ee9b487eb2
maliByatzes <malib2027@gmail.com>
2023-05-11 07:55:47 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,21 @@ function App() {
 
   return (
     <>
-      <h1>Hello World!</h1>
+      <div className='navbar__div'>
+        <div className='navbar__div-links'>
+          <img src='../images/logo.svg' />
+          <ul>
+            <li>Features <img src='../images/icon-arrow-down.svg' /></li>
+            <li>Company <img src='../images/icon-arrow-down.svg' /></li>
+            <li>Careers</li>
+            <li>About</li>
+          </ul>
+        </div>
+        <div className='navbar__div-buttons'>
+          <p>Login</p>
+          <button type='button'>Register</button>
+        </div>
+      </div>
     </>
   )
 }
